f37af1f3c749972719ac9ca347a563c26f732eb2
leeann <leeann.lauter@gmail.com>
2018-01-24 20:18:50 -0600

    If, elif and else working with integers.

--- Lesson1.py
+++ Lesson1.py
@@ -3,6 +3,10 @@ from pip._vendor.distlib.compat import raw_input
 number = int(raw_input("Number: "))
 print(number)
 if number > 5:
+    if number == 10:
+        print("You are special")
+    else:
+        print("You are not special")
     print("Number is greater than 5")
 elif number < 5:
     print("Number is less than 5")
